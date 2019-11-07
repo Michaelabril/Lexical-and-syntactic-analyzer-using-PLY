c300363cfa124c8db7d54ce8247cb0b4ba73c7c2
Michael abril <maiicolabriil@hotmail.com>
2019-11-07 13:45:44 -0500

    Uploading the completed project

---
+++ analizador_sintactico.py
@@ -0,0 +1,173 @@
+import ply.yacc as yacc
+from analizador_lexico import tokens
+from analizador_lexico import analizador
+
+# resultado del analisis
+resultado_gramatica = []
+
+precedence = (
+    ('right','ASIGNAR'),
+    ('left', 'SUMA', 'RESTA'),
+    ('left', 'MULT', 'DIV'),
+    ('right', 'UMINUS'),
+)
+nombres = {}
+
+def p_declaracion_asignar(t):
+    'declaracion : IDENTIFICADOR ASIGNAR expresion PUNTOCOMA'
+    nombres[t[1]] = t[3]
+
+def p_declaracion_expr(t):
+    'declaracion : expresion'
+    # print("Resultado: " + str(t[1]))
+    t[0] = t[1]
+
+def p_expresion_operaciones(t):
+    '''
+    expresion  :   expresion SUMA expresion
+                |   expresion RESTA expresion
+                |   expresion MULT expresion
+                |   expresion DIV expresion
+                |   expresion POTENCIA expresion
+                |   expresion MODULO expresion
+
+    '''
+    if t[2] == '+':
+        t[0] = t[1] + t[3]
+    elif t[2] == '-':
+        t[0] = t[1] - t[3]
+    elif t[2] == '*':
+        t[0] = t[1] * t[3]
+    elif t[2] == '/':
+        t[0] = t[1] / t[3]
+    elif t[2] == '%':
+        t[0] = t[1] % t[3]
+    elif t[2] == '**':
+        i = t[3]
+        t[0] = t[1]
+        while i > 1:
+            t[0] *= t[1]
+            i -= 1
+
+def p_expresion_uminus(t):
+    'expresion : RESTA expresion %prec UMINUS'
+    t[0] = -t[2]
+
+def p_expresion_grupo(t):
+    '''
+    expresion  : PARIZQ expresion PARDER
+                | LLAIZQ expresion LLADER
+                | CORIZQ expresion CORDER
+    '''
+    t[0] = t[2]
+# sintactico de expresiones logicas
+def p_expresion_logicas(t):
+    '''
+    expresion   :  expresion MENORQUE expresion 
+                |  expresion MAYORQUE expresion 
+                |  expresion MENORIGUAL expresion 
+                |   expresion MAYORIGUAL expresion 
+                |   expresion IGUAL expresion 
+                |   expresion DISTINTO expresion
+                |  PARIZQ expresion PARDER MENORQUE PARIZQ expresion PARDER
+                |  PARIZQ expresion PARDER MAYORQUE PARIZQ expresion PARDER
+                |  PARIZQ expresion PARDER MENORIGUAL PARIZQ expresion PARDER 
+                |  PARIZQ  expresion PARDER MAYORIGUAL PARIZQ expresion PARDER
+                |  PARIZQ  expresion PARDER IGUAL PARIZQ expresion PARDER
+                |  PARIZQ  expresion PARDER DISTINTO PARIZQ expresion PARDER
+    '''
+    if t[2] == "<": t[0] = t[1] < t[3]
+    elif t[2] == ">": t[0] = t[1] > t[3]
+    elif t[2] == "<=": t[0] = t[1] <= t[3]
+    elif t[2] == ">=": t[0] = t[1] >= t[3]
+    elif t[2] == "==": t[0] = t[1] is t[3]
+    elif t[2] == "!=": t[0] = t[1] != t[3]
+    elif t[3] == "<":
+        t[0] = t[2] < t[4]
+    elif t[2] == ">":
+        t[0] = t[2] > t[4]
+    elif t[3] == "<=":
+        t[0] = t[2] <= t[4]
+    elif t[3] == ">=":
+        t[0] = t[2] >= t[4]
+    elif t[3] == "==":
+        t[0] = t[2] is t[4]
+    elif t[3] == "!=":
+        t[0] = t[2] != t[4]
+
+    # print('logica ',[x for x in t])
+
+# gramatica de expresiones booleanadas
+def p_expresion_booleana(t):
+    '''
+    expresion   :   expresion AND expresion 
+                |   expresion OR expresion 
+                |   expresion NOT expresion 
+                |  PARIZQ expresion AND expresion PARDER
+                |  PARIZQ expresion OR expresion PARDER
+                |  PARIZQ expresion NOT expresion PARDER
+    '''
+    if t[2] == "&&":
+        t[0] = t[1] and t[3]
+    elif t[2] == "||":
+        t[0] = t[1] or t[3]
+    elif t[2] == "!":
+        t[0] =  t[1] is not t[3]
+    elif t[3] == "&&":
+        t[0] = t[2] and t[4]
+    elif t[3] == "||":
+        t[0] = t[2] or t[4]
+    elif t[3] == "!":
+        t[0] =  t[2] is not t[4]
+
+def p_expresion_numero(t):
+    'expresion : ENTERO'
+    t[0] = t[1]
+
+def p_expresion_cadena(t):
+    'expresion : COMDOB expresion COMDOB'
+    t[0] = t[2]
+
+def p_expresion_nombre(t):
+    'expresion : IDENTIFICADOR'
+    try:
+        t[0] = nombres[t[1]]
+    except LookupError:
+        print("Nombre desconocido ", t[1])
+        t[0] = 0
+
+def p_error(t):
+    global resultado_gramatica
+    if t:
+        resultado = "Error sintactico de tipo {} en el valor {}".format( str(t.type),str(t.value))
+        print(resultado)
+    else:
+        resultado = "Error sintactico {}".format(t)
+        print(resultado)
+    resultado_gramatica.append(resultado)
+
+# instanciamos el analizador sistactico
+parser = yacc.yacc()
+
+def prueba_sintactica(data):
+    global resultado_gramatica
+    resultado_gramatica.clear()
+
+    for item in data.splitlines():
+        if item:
+            gram = parser.parse(item)
+            if gram:
+                resultado_gramatica.append(str(gram))
+        else: print("data vacia")
+
+    print("result: ", resultado_gramatica)
+    return resultado_gramatica
+
+if __name__ == '__main__':
+    while True:
+        try:
+            s = input(' ingresa dato >>> ')
+        except EOFError:
+            continue
+        if not s: continue
+        prueba_sintactica(s)
---
+++ main.py
@@ -0,0 +1,112 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.uic.properties import QtGui
+
+""" Importamos todas nuetras Ventana y funciones utiles"""
+from vista.home import  *
+from analizador_lexico import *
+from analizador_sintactico import *
+
+class Main(QMainWindow):
+    """ Clase principal de nuestra app"""
+    def __init__(self):
+        """ Incializamos nuestra app"""
+        QMainWindow.__init__(self)
+
+        # Instaciamos nuestra ventanas widget home
+        self.home = Ui_home()
+        self.home.setupUi(self)
+
+        # Eventos
+        self.home.bt_lexico.clicked.connect(self.ev_lexico)
+        self.home.bt_sintactico.clicked.connect(self.ev_sintactico)
+
+        self.home.bt_archivo.clicked.connect(self.ev_archivo)
+        self.home.bt_limpiar.clicked.connect(self.ev_limpiar)
+
+        #Desarrollandores
+        self.home.estado.showMessage("Desarrollado por Maryon Torres y Michael Abril")
+
+    def ev_lexico(self):
+        '''
+        Manejo de analisis de expresion lexemas
+        :return: 
+        '''
+
+        # limpiamos el campo
+        self.home.tx_lexico.setText('')
+
+        #Obtenemos los datos ingresados
+        datos = self.home.tx_ingreso.toPlainText().strip()
+
+        # analizamos la lexemas de los datos ingresados
+        resultado_lexico = prueba(datos)
+
+       # self.home.tx_lexico.setText("Analizando lexico")
+        cadena= ''
+        for lex in resultado_lexico:
+            cadena += lex + "\n"
+        self.home.tx_lexico.setText(cadena)
+
+    def ev_sintactico(self):
+        '''
+        Manejo de analisis gramatico
+        :return: 
+        '''
+        # print("sintactico")
+
+        # limpiamos el campo
+        self.home.tx_sintactico.setText('')
+        #Obtenemos los datos ingresados
+        datos = self.home.tx_ingreso.toPlainText().strip()
+
+        #analizamos la gramatica de los datos ingresados
+        resultado_sintactico = prueba_sintactica(datos)
+        cadena = ''
+
+        #Armanos la cadena a mostrar
+        for item in resultado_sintactico:
+            cadena += item + "\n"
+        # mostramos en pantalla
+        self.home.tx_sintactico.setText( cadena )
+
+    def ev_archivo(self):
+        '''
+        Manejo de subir archivo 
+        :return: 
+        '''
+        dlg = QFileDialog()
+
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            f = open(filenames[0], 'r')
+
+            with f:
+                data = f.read().strip()
+                if data:
+                    self.home.tx_ingreso.setText(data+"\n")
+
+    def ev_limpiar(self):
+        '''
+        Manejo de limpieza de campos
+        :return: 
+        '''
+        self.home.tx_ingreso.setText('')
+        self.home.tx_lexico.setText('')
+        self.home.tx_sintactico.setText('')
+
+def iniciar():
+    # Instaciamos nuestro app por defecto esto no cambia
+    app = QApplication(sys.argv)
+
+    # Instaciomos nuestro ventana
+    ventana = Main()
+    # Mostramos nuestra app
+    ventana.show()
+
+    #Controlamos el cierre de la app
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    iniciar()
